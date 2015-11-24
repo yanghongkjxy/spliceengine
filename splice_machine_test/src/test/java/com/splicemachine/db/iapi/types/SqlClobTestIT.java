7c16323d122387b55dc37c5891b028541db99b7c
Dmitry Ochnev <dochnev@splicemachine.com>
2015-11-24 11:13:39 -0600

    Added ITs for Boolean, Char, Small Int and CLOB

---
+++ splice_machine_test/src/test/java/com/splicemachine/db/iapi/types/SqlClobTestIT.java
@@ -0,0 +1,111 @@
+package com.splicemachine.db.iapi.types;
+
+import org.junit.*;
+
+import javax.sql.rowset.serial.SerialClob;
+import java.sql.Clob;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+/**
+ * Created by ochnev on 11/23/15.
+ */
+public class SqlClobTestIT extends DataTypeIT {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        SqlClobTestIT it = new SqlClobTestIT();
+        it.createTable();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        watcher.closeAll();
+    }
+
+    @Before
+    public void beforeTest() throws Exception {
+        runDelete(); // clear the table before each test
+    }
+
+    @Override
+    protected String getTableName() {
+        return "clob_it";
+    }
+
+    @Override
+    protected String getSqlDataType() {
+        return "clob";
+    }
+
+    @Override
+    protected void setParameterValue(PreparedStatement preparedStatement, Object value, int paramIndex) throws SQLException {
+        if (value == null) {
+            preparedStatement.setNull(1, Types.CLOB);
+            return;
+        }
+
+        if (value instanceof Clob) {
+            Clob clob = (Clob) value;
+            preparedStatement.setClob(paramIndex, clob);
+            return;
+        }
+
+        throw new IllegalArgumentException("Not supported type: " + value.getClass().getCanonicalName());
+    }
+
+    @Test
+    @Override
+    public void testMinValue() throws Exception {
+        Clob testClob = new SerialClob(new char[] {'a'});
+        runInsert(testClob);
+    }
+
+    @Ignore
+    @Test
+    @Override
+    public void testMaxValue() throws Exception {
+        // Too huge to test it on a developer's machine
+    }
+
+    @Test
+    @Override
+    public void testNormalValue() throws Exception {
+        Clob testClob = getSomeClob(100_000);
+        runInsert(testClob);
+    }
+
+    @Ignore
+    @Test
+    @Override
+    public void testBiggerMaxNegative() throws Exception {
+    }
+
+    @Ignore
+    @Test
+    @Override
+    public void testSmallerMinNegative() throws Exception {
+    }
+
+    @Test
+    @Override
+    public void testNullValue() throws Exception {
+        runInsert(null);
+    }
+
+    private Clob getSomeClob(int size) throws SQLException {
+        char[] ch = new char[size];
+        for(int i = 0; i < size; i++) {
+            ch[i] = 'a';
+        }
+        return new SerialClob(ch);
+    }
+
+    @Ignore
+    @Test
+    @Override
+    public void testUpdateValue() throws Exception {
+    }
+
+}
