16a5b8770866cb95f24f63018be22a41b52d79b6
John Leach <jleach@splicemachine.com>
2016-11-09 14:26:13 -0600

    EFS Test

---
+++ splice_machine/src/main/java/org/apache/hadoop/fs/EFSFileSystem.java
@@ -0,0 +1,52 @@
+package org.apache.hadoop.fs;
+
+/**
+ * Created by jleach on 11/9/16.
+ */
+import com.google.common.annotations.VisibleForTesting;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutput;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.FileDescriptor;
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.file.AccessDeniedException;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.nativeio.NativeIO;
+import org.apache.hadoop.util.Progressable;
+import org.apache.hadoop.util.Shell;
+import org.apache.hadoop.util.StringUtils;
+
+/****************************************************************
+ * Implement the FileSystem API for the raw local filesystem.
+ *
+ *****************************************************************/
+@InterfaceAudience.Public
+@InterfaceStability.Stable
+public class EFSFileSystem extends RawLocalFileSystem {
+    static final URI NAME = URI.create("efs:///");
+    @Override
+    public URI getUri() {
+        return NAME;
+    }
+
+    @Override
+    public String getScheme() {
+        return "efs";
+    }
+}
---
+++ splice_machine/src/test/java/org/apache/hadoop/fs/EHFileSystemTest.java
@@ -0,0 +1,21 @@
+package org.apache.hadoop.fs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Test;
+
+import java.net.URI;
+
+/**
+ * Created by jleach on 11/9/16.
+ */
+public class EHFileSystemTest {
+
+    @Test
+    public void testEFSFileSystem() throws Exception {
+
+        FileSystem fileSystem = FileSystem.get(new URI("file:///"),new Configuration());
+
+        FileStatus fileStatus = fileSystem.getFileStatus(new Path("file:///Users/jleach/Ugh.jps"));
+        System.out.println(fileStatus.getOwner());
+    }
+}
