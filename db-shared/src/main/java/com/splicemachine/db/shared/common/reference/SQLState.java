23640527876188faacb99cc3b7d110f11a4ea76c
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2017-09-07 18:27:31 +0200

    SPLICE-175 External Olap server

--- db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
+++ db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
@@ -2009,5 +2009,7 @@ public interface SQLState {
 	String REGION_DOESNOT_EXIST 									= "TS003";
 	String WRONG_REGION_NAME_TYPE 									= "TS004";
 	String REGION_NOT_ADJACENT                                      = "TS005";
+
+	String OLAP_SERVER_CONNECTION                                   = "OS001";
 }
 
